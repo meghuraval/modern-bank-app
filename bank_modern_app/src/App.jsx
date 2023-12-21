531682523370a41c11c8fa4d9159adaaa3b24b3d
meghu <meghuraval1234@yahoo.com>
2023-12-21 12:50:55 -0500

    components files added, created index.js file within components folder to export effeciently, working so far

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,7 +1,52 @@
-import "./App.css";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import styles from "./style";
 
-function App() {
-  return <></>;
-}
+import {
+  BillingDetails,
+  Business,
+  //Button,
+  CardDeal,
+  Clients,
+  Cta,
+  //FeedbackCard,
+  Footer,
+  //GetStarted,
+  Hero,
+  NavBar,
+  Stats,
+  Testimonials,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
+      </div>
+
+      <div>
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats /> <Business /> <BillingDetails /> <CardDeal />{" "}
+            <Testimonials />
+            <Clients /> <Cta />
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ bank_modern_app/src/components/index.js
@@ -0,0 +1,29 @@
+import BillingDetails from "./BillingDetails";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Cta from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  BillingDetails,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  Cta,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  NavBar,
+  Stats,
+  Testimonials,
+};
