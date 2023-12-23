2d6dcc99ea9462edb188e2213e7b9878241b0725
meghu <meghuraval1234@yahoo.com>
2023-12-23 13:28:40 -0500

    finished business section and the button section along with feature card section

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -1,8 +1,16 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`${styles} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none hover:scale-105 rounded-lg`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
