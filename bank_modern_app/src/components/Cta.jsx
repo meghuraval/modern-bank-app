3789d9192a9839b6c73cbccc5c47180731df463b
meghu <meghuraval1234@yahoo.com>
2023-12-23 21:55:49 -0500

    added CTA sucesfully

--- bank_modern_app/src/components/Cta.jsx
+++ bank_modern_app/src/components/Cta.jsx
@@ -1,8 +1,27 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import styles from "../style";
+import Button from "./Button";
 
 const Cta = () => {
-  return <div>CTA</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    >
+      <div className="flex-1 flex flex-col">
+        <h2 style={styles.heading2}>Try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Get ready to supercharge your business growth by accepting card
+          payments from every corner of the planet! All the tools you need are
+          right here.
+        </p>
+      </div>
+
+      <div className={`${styles.flexCenter} sm:m1-10 ml-0 sm:mt-0 mt-10`}>
+        <Button />
+      </div>
+    </section>
+  );
 };
 
 export default Cta;
