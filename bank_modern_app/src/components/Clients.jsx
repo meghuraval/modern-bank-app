908039251e97acd36fad46d79011c913423491d8
meghu <meghuraval1234@yahoo.com>
2023-12-23 21:44:21 -0500

    created companies worked for logos succesfully

--- bank_modern_app/src/components/Clients.jsx
+++ bank_modern_app/src/components/Clients.jsx
@@ -1,8 +1,27 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import React from "react";
+import styles from "../style";
+import { clients } from "../constants";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+          >
+            <img
+              src={client.logo}
+              alt="client"
+              className="sm:w-[192px] w-[100px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
